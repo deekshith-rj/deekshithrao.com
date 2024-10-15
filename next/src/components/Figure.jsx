f27d9151f886740d90eb5ca0dad9b99fd6467a48
Deekshith Rao <8099566+deekshith-rj@users.noreply.github.com>
2024-10-15 13:02:14 +0530

    Setting the NEXT_PUBLIC_BACKEND_URL value correctly in .env file fixed it. But there's a double slash in the media request from frontend. Removed it though not an issue in most cases.

--- next/src/components/Figure.jsx
+++ next/src/components/Figure.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Figure = props => {
-
+const Figure = (props) => {
   const { figure } = props;
 
   return (
     <figure className="w-full max-w-3xl mx-auto">
-      <img src={`${process.env.NEXT_PUBLIC_MEDIA_URL}/${figure.image}`} className="w-full" />
+      <img
+        src={`${process.env.NEXT_PUBLIC_MEDIA_URL}/${figure.image}`}
+        className="w-full"
+      />
       {figure.caption && (
         <figcaption
-          dangerouslySetInnerHTML={{__html: figure.caption}}
+          dangerouslySetInnerHTML={{ __html: figure.caption }}
           className="text-center text-slate-500 text-xs sm:text-sm md:text-base"
         />
       )}
@@ -19,7 +21,7 @@ const Figure = props => {
 };
 
 Figure.propTypes = {
-  figure: PropTypes.object.isRequired
+  figure: PropTypes.object.isRequired,
 };
 
 export default Figure;
